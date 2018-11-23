a384925793ded081ae85bb798a1a50136f997edf
Ratnadeep Bhattacharyya <ratnadeepronnie@gmail.com>
2018-11-24 01:26:33 +0530

    Added constructor

--- calc.ts
+++ calc.ts
@@ -2,6 +2,11 @@ export class Calculator {
   a: number;
   b: number;
 
+  constructor(num1: number, num2: number) {
+    this.a = num1;
+    this.b = num2;
+  }
+
   add(): number {
     return this.a + this.b;
   }
